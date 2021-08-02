2fb881ae8d29cce9dfcccca9e0390199269246ae
David Spindler <ashleywoods@Ashleys-MacBook-Pro.local>
2021-08-02 10:32:45 -0700

    first commit

---
+++ src/constants/constants.js
@@ -0,0 +1,45 @@
+export const projects = [
+  {
+    title: 'Photography Website',
+    description: "Using React, HTML, CSS & JS this website features responsive design and has boosted the photographer's client outreach by over 200%.",
+      image: './images/screenshotAliWebsite.png',
+      tags: ['React', 'CSS', 'Javascript', 'HTML'],
+    demo: 'https://alimohamed.netlify.app/',
+    visit: 'https://github.com/davidspindler/alimohamedphotography',
+    id: 0,
+  },
+  {
+    title: 'Calculator',
+    description:"Utilizing JavaScript and CSS, this is a website that users can input any integers and do basic calculations",
+    image: '/images/calculator.jpeg',
+    tags: ['CSS', 'JavaScript'],
+    demo: 'https://calculatorexample.netlify.app/',
+    visit: 'https://github.com/davidspindler/CalculatorEx/tree/main',
+    id: 1,
+  },
+  {
+    title: 'Dog-Image API',
+    description: "This is a website for a simple button that requests images from an API at dog.ceo",
+    image: '/images/dogapiimage.png',
+    tags: ['NODE', 'API'],
+    demo: 'https://dogimage.netlify.app/',
+    visit: 'https://github.com/davidspindler/Dogimages',
+    id: 2,
+  },
+  {
+    title: 'TRVL WEBSITE',
+    description: "Using React, HTML, CSS & JS to display a website that users can look at places they would want to visit.",
+    image: '/images/adventureawaits1.png',
+    tags: ['React', 'CSS'],
+    demo: 'https://demotravelwebsitedavidspindler.netlify.app/',
+    visit: 'https://github.com/davidspindler/DemoTravelWebsite',
+    id: 3,
+  },
+];
+
+export const TimeLineData = [
+  { year: 2018, text: 'Graduated with B.S. in Mathematics', },
+  { year: 2019, text: 'Learned foundations of Computer Science', },
+  { year: 2020, text: 'Worked on Front End Web Development', },
+  { year: 2021, text: 'Freelance Web Developer', },
+];
